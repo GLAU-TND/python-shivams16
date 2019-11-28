e7977a0ee3354797251c3332f65392abf9643d57
shivams16 <shivamkumar8468@gmail.com>
2019-11-28 19:46:01 +0530

    added solution1 assignment

---
+++ solution 1.py
@@ -0,0 +1,15 @@
+class Myexception(Exception):
+    def __init__(self,v):
+        self.v=v
+    def __str__(self):
+        return str(self.v)
+
+def xyz(a,b):
+    c=a+b
+    if c < 150:
+        raise Myexception('custom exception occured')
+    else:
+        return c
+a = int(input())
+b=int(input())
+print(xyz(a, b))
